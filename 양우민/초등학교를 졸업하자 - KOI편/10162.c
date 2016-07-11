383299463765f20e3f3d0795f85cfa2a6812113f
YangWoomin <cicyowoo@naver.com>
2016-07-11 22:13:10 +0900

    yang2

---
+++ 양우민/초등학교를 졸업하자 - KOI편/10162.c
@@ -0,0 +1,23 @@
+#include "header.h"
+
+void _10162() {
+	int i;
+	int inputNum, temp;
+	int divider[3] = { 300, 60, 10 };
+	int result[3] = { 0 };
+
+	scanf("%d", &inputNum);
+	temp = inputNum;
+	for (i = 0; i < 3; i++) {
+		result[i] = temp / divider[i];
+		temp = temp - result[i] * divider[i];
+	}
+	if (temp) {
+		printf("-1");
+	}
+	else {
+		for (i = 0; i < 3; i++) {
+			printf("%d ", result[i]);
+		}
+	}
+}
--- 양우민/초등학교를 졸업하자 - KOI편/main.c
+++ 양우민/초등학교를 졸업하자 - KOI편/main.c
@@ -1,6 +1,6 @@
 #include "header.h"
 
 int main() {
-	_10163();
+	_10162();
 	return 0;
 }
