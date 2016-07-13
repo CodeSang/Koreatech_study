6716c215ee14e691e72fa97d5295a7eb07ed09b8
YangWoomin <cicyowoo@naver.com>
2016-07-13 17:22:25 +0900

    yang3

--- 양우민/초등학교를 졸업하자 - KOI편/8982.c
+++ 양우민/초등학교를 졸업하자 - KOI편/8982.c
@@ -1,9 +1,11 @@
 #include "header.h"
 
 void _8982() {
-	int i;
+	int i, j;
 	int inputNum;
 	int **point;
+	int drain;
+	int **dPoint;
 
 	scanf("%d", &inputNum);
 	point = (int**)malloc(sizeof(int*) * inputNum);
@@ -11,6 +13,15 @@ void _8982() {
 		point[i] = (int*)malloc(sizeof(int) * 2);
 		scanf("%d %d", &point[i][0], &point[i][1]);
 	}
+	scanf("%d", &drain);
+	dPoint = (int**)malloc(sizeof(int*) * drain);
+	for (i = 0; i < drain; i++) {
+		dPoint[i] = (int*)malloc(sizeof(int) * 4);
+		for (j = 0; j < 4; j++) {
+			scanf("%d", &dPoint[i][j]);
+		}
+	}
+
 	for (i = 0; i < inputNum; i++) {
 		printf("%d %d\n", point[i][0], point[i][1]);
 		free(point[i]);
