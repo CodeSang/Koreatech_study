259b212e390ffe04e8989c21b3383d5159990d8f
Jaeyeong-Lee <w22ee1@naver.com>
2016-07-14 00:57:48 +0900

    U

---
+++ 이재영/완전탐색/src/_9663_NQueen/Main.java
@@ -0,0 +1,16 @@
+package _9663_NQueen;
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+	
+		int n = sc.nextInt();
+		System.out.println(n);
+	}
+	
+	
+}
