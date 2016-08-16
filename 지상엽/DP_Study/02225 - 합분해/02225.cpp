c811ab434a9f321205b6d3d170c6229bfbc30a7d
CodeSang <tkdduq9915@koreatech.ac.kr>
2016-08-16 09:42:27 +0900

    A
    A

--- 지상엽/DP_Study/02225 - 합분해/02225.cpp
+++ 지상엽/DP_Study/02225 - 합분해/02225.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 
 int N, K;
-int dp[202] = { 1 };
+int dp[202][202]; // N,K
+
+int Go(int n, int k)
+{
+	if (k == 1) return 1;
+	int& temp = dp[n][k];
+	if (temp != 0) return temp;
+	for (int i = 0; i <= n; i++)
+		temp = (temp + Go(n - i, k - 1) ) % LIM;
+	return temp;
+}
+
 int main() {
 	cin >> N >> K;
-
-	for (int i = 1; i < N; i++)
-	{
-
-
-
-
-	}
+	cout << Go(N, K) << endl;
 }
